b213b12c13aff1aef2cecdfa9231ce97dd260131
User <User@DESKTOP-OGD5845.mshome.net>
2022-04-05 16:47:14 -0500

    Creación Modelos User y Bill

---
+++ springboot-skill-test/src/main/java/com/springtest/models/BillModel.java
@@ -0,0 +1,57 @@
+package com.springtest.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class BillModel {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name="totalAmount")
+	private double totalAmount;
+	
+	@Column(name="desc")
+	private String desc;
+	
+	@ManyToOne
+	@JoinColumn(name="id_user")
+	private UserModel id_user;
+	
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public double getTotalAmount() {
+		return totalAmount;
+	}
+	public void setTotalAmount(double totalAmount) {
+		this.totalAmount = totalAmount;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public UserModel getId_user() {
+		return id_user;
+	}
+	public void setId_user(UserModel id_user) {
+		this.id_user = id_user;
+	}
+	
+	
+	
+}
---
+++ springboot-skill-test/src/main/java/com/springtest/models/UserModel.java
@@ -0,0 +1,53 @@
+package com.springtest.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UserModel {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="age")
+	private int age;
+	
+	@Column(name="email")
+	private String email;
+	
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
