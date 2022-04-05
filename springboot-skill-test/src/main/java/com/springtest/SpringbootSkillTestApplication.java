f72675401ddcd353c92588b84214ededfe35bc74
User <User@DESKTOP-OGD5845.mshome.net>
2022-04-05 15:40:20 -0500

    V 0.1 - Creación de proyecto

---
+++ springboot-skill-test/src/main/java/com/springtest/SpringbootSkillTestApplication.java
@@ -0,0 +1,13 @@
+package com.springtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSkillTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSkillTestApplication.class, args);
+	}
+
+}
